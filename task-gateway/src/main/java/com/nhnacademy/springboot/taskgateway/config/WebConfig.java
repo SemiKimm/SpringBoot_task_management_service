1ef221e21879ada216d6faa2500111be10359fd4
semi <semikimm.m@gmail.com>
2022-06-11 19:28:56 +0900

    [task-management-gateway] spring-security 로그인 & redis 에 session id & userName(id) & authority 저장 구현 완료

--- task-gateway/src/main/java/com/nhnacademy/springboot/taskgateway/config/WebConfig.java
+++ task-gateway/src/main/java/com/nhnacademy/springboot/taskgateway/config/WebConfig.java
@@ -24,5 +24,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
+        registry.addViewController("/signIn").setViewName("auth/loginForm");
     }
 }
