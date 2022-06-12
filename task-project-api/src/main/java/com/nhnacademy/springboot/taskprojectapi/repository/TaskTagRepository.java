4ed46094980c34518c3a7a4e8d479d50728a2b98
semi <semikimm.m@gmail.com>
2022-06-13 05:52:23 +0900

    [task-management-project-api] tag 삭제 할때 task tag 에 있는것도 삭제되도록 수정 완료

--- task-project-api/src/main/java/com/nhnacademy/springboot/taskprojectapi/repository/TaskTagRepository.java
+++ task-project-api/src/main/java/com/nhnacademy/springboot/taskprojectapi/repository/TaskTagRepository.java
@@ -5,12 +5,19 @@ import com.nhnacademy.springboot.taskprojectapi.entity.Task;
 import com.nhnacademy.springboot.taskprojectapi.entity.TaskTag;
 import com.nhnacademy.springboot.taskprojectapi.entity.pk.TaskTagPk;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface TaskTagRepository extends JpaRepository<TaskTag, TaskTagPk> {
     @Query("select tt from TaskTag tt where tt.task = :task")
     List<TaskTagDto> findTaskTagDtoBy(@Param("task") Task task);
+
+    @Transactional
+    @Modifying
+    @Query("delete from TaskTag tt where tt.tag.no = :tagNo")
+    Integer deleteByTag(@Param("tagNo") Integer tagNo);
 }
--- task-project-api/src/main/java/com/nhnacademy/springboot/taskprojectapi/service/impl/TagServiceImpl.java
+++ task-project-api/src/main/java/com/nhnacademy/springboot/taskprojectapi/service/impl/TagServiceImpl.java
@@ -5,6 +5,7 @@ import com.nhnacademy.springboot.taskprojectapi.entity.Project;
 import com.nhnacademy.springboot.taskprojectapi.entity.Tag;
 import com.nhnacademy.springboot.taskprojectapi.repository.ProjectRepository;
 import com.nhnacademy.springboot.taskprojectapi.repository.TagRepository;
+import com.nhnacademy.springboot.taskprojectapi.repository.TaskTagRepository;
 import com.nhnacademy.springboot.taskprojectapi.service.TagService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,7 @@ import java.util.List;
 public class TagServiceImpl implements TagService {
     private final TagRepository tagRepository;
     private final ProjectRepository projectRepository;
+    private final TaskTagRepository taskTagRepository;
 
     @Override
     public Tag register(Integer projectNo, String name) {
@@ -37,13 +39,14 @@ public class TagServiceImpl implements TagService {
     }
 
     @Override
-    public String deleteTag(Integer tagNo) { // fixme : task tag 에 있는것도 삭제되도록 하기
+    public String deleteTag(Integer tagNo) {
         if(!tagRepository.existsById(tagNo)){
             throw new IllegalStateException("not exist tag : " + tagNo);
         }
+        Integer deletedTaskTagCount =  taskTagRepository.deleteByTag(tagNo);
         tagRepository.deleteById(tagNo);
 
-        return "{\"result\":\"delete success\"}";
+        return "{\"result\":\"delete success\", \"deletedTaskTagCount\":"+deletedTaskTagCount+"}";
     }
 
     @Override
